42f1827f451f8724e7f0d7efa7c22e13bb51b545
Venkata Krishnan Anantha Raman <venkatakrishnansvpr@gmail.com>
2018-04-05 00:16:31 -0400

    containskey api implemented

--- app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -111,6 +112,23 @@ public class SimpleDhtProvider extends ContentProvider {
         return message;
     }
 
+    public boolean containskey (String selection) {
+        InputStream is = null;
+        try {
+            is = getContext().openFileInput(selection);
+            is.close();
+            return true;
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         Log.d("venkat","delete "+selection+" "+selectionArgs);
